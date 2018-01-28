e9c9fe2d6e156167e3bfaa401dcd34475c73d8bb
grigory <grigory08@ya.ru>
2018-01-29 00:06:19 +0300

    Уменьшил количество файлов представления.

--- views/layouts/indexTemplate.php
+++ views/layouts/indexTemplate.php
@@ -8,7 +8,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <title><?php $data['title']; ?></title>
+        <title><?php echo($data['title']); ?></title>
     </head>
     <body>
         <div class="container">
